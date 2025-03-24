1418c9a5766c02b7fcc43d7e02271bf765eccd8a
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-03-24 22:00:26 +0200

    feat(config): add HTML validation support and update app metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: 'Nuxt App',
+      meta: [
+        { name: 'description', content: 'Nuxt App' },
+      ],
+    },
+  },
+
   css: ['~/assets/css/main.css'],
 
   modules: [
@@ -12,6 +21,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxtjs/i18n',
+    '@nuxtjs/html-validator'
   ],
 
   vite: {
@@ -28,6 +38,33 @@ export default defineNuxtConfig({
     lazy: false,
     langDir: 'locales',
     strategy: 'no_prefix',
+  },
 
+  htmlValidator: {
+    usePrettier: true,
+    logLevel: 'verbose',
+    failOnError: false,
+    /** A list of routes to ignore (that is, not check validity for). */
+    ignore: [/\.(xml|rss|json)$/],
+    options: {
+      extends: [
+        'html-validate:document',
+        'html-validate:recommended',
+        'html-validate:standard'
+      ],
+      rules: {
+        'svg-focusable': 'off',
+        'no-unknown-elements': 'error',
+        // Conflicts or not needed as we use prettier formatting
+        'void-style': 'off',
+        'no-trailing-whitespace': 'off',
+        // Conflict with Nuxt defaults
+        'require-sri': 'off',
+        'attribute-boolean-style': 'off',
+        'doctype-style': 'off',
+        // Unreasonable rule
+        'no-inline-style': 'off'
+      }
+    }
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+
+const { t, locale } = useI18n()
+
+useHead({
+    htmlAttrs: {
+        lang: locale.value
+    }
+})
+
+</script>
+
 <template>
     <div>
-        <h1>{{ $t('hi') }}</h1>
+        <h1>{{ t('hi') }}</h1>
     </div>
 </template>
