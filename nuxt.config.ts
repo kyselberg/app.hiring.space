f4d44d1e8efd7018980ce2a23aa8901071fbd588
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-03-24 22:01:49 +0200

    chore(config): add Prettier configuration and update formatting across files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,70 +1,64 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   app: {
     head: {
-      title: 'Nuxt App',
-      meta: [
-        { name: 'description', content: 'Nuxt App' },
-      ],
+      title: "Nuxt App",
+      meta: [{ name: "description", content: "Nuxt App" }],
     },
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxtjs/i18n',
-    '@nuxtjs/html-validator'
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxtjs/i18n",
+    "@nuxtjs/html-validator",
   ],
 
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 
   i18n: {
-    locales: [
-      { code: 'en', language: 'en-US', iso: 'en-US', file: 'en.json' },
-    ],
-    defaultLocale: 'en',
+    locales: [{ code: "en", language: "en-US", iso: "en-US", file: "en.json" }],
+    defaultLocale: "en",
     lazy: false,
-    langDir: 'locales',
-    strategy: 'no_prefix',
+    langDir: "locales",
+    strategy: "no_prefix",
   },
 
   htmlValidator: {
     usePrettier: true,
-    logLevel: 'verbose',
+    logLevel: "verbose",
     failOnError: false,
     /** A list of routes to ignore (that is, not check validity for). */
     ignore: [/\.(xml|rss|json)$/],
     options: {
       extends: [
-        'html-validate:document',
-        'html-validate:recommended',
-        'html-validate:standard'
+        "html-validate:document",
+        "html-validate:recommended",
+        "html-validate:standard",
       ],
       rules: {
-        'svg-focusable': 'off',
-        'no-unknown-elements': 'error',
+        "svg-focusable": "off",
+        "no-unknown-elements": "error",
         // Conflicts or not needed as we use prettier formatting
-        'void-style': 'off',
-        'no-trailing-whitespace': 'off',
+        "void-style": "off",
+        "no-trailing-whitespace": "off",
         // Conflict with Nuxt defaults
-        'require-sri': 'off',
-        'attribute-boolean-style': 'off',
-        'doctype-style': 'off',
+        "require-sri": "off",
+        "attribute-boolean-style": "off",
+        "doctype-style": "off",
         // Unreasonable rule
-        'no-inline-style': 'off'
-      }
-    }
-  }
-})
+        "no-inline-style": "off",
+      },
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-
-const { t, locale } = useI18n()
+const { t, locale } = useI18n();
 
 useHead({
-    htmlAttrs: {
-        lang: locale.value
-    }
-})
-
+  htmlAttrs: {
+    lang: locale.value,
+  },
+});
 </script>
 
 <template>
-    <div>
-        <h1>{{ t('hi') }}</h1>
-    </div>
+  <div>
+    <h1>{{ t("hi") }}</h1>
+  </div>
 </template>
